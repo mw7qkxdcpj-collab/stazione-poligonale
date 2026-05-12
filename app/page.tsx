0097ad0eb69cb75a7c73fb452050b14fa659c03e
Maurizio <maurizio1999@hotmail.it>
2026-05-12 19:37:20 +0200

    Modifica messaggio

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen bg-white text-gray-900">
-      <h1>Blog in costruzione</h1>
+      <h1>Blog in costruzione...</h1>
     </main>
   );
 }
