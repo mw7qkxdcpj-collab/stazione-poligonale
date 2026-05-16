dcca27e4712de05bc881ca6a6ce260a265bf55c5
mw7qkxdcpj-collab <maurizio1999@hotmail.it>
2026-05-16 18:42:10 +0200

    navbar più logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@
 import "./globals.css";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import Image from "next/image"
 
 export default function RootLayout({
   children,
@@ -18,9 +19,21 @@ export default function RootLayout({
         {/* NAVBAR */}
         <nav className="flex items-center justify-between px-8 py-4 border-b border-zinc-800 bg-black/70 backdrop-blur-md sticky top-0 z-50">
 
-          <div className="text-green-400 font-bold text-xl tracking-widest hover:scale-105 transition-transform duration-300">
-            STAZIONE POLIGONALE
-          </div>
+          <div className="flex items-center gap-3 hover:scale-105 transition-transform duration-300">
+  
+  <Image
+    src="/logo.png"
+    alt="Logo"
+    width={32}
+    height={32}
+    className="rounded-full"
+  />
+
+  <span className="text-white-400 font-bold text-xl tracking-widest">
+    STAZIONE POLIGONALE
+  </span>
+
+</div>
 
           <div className="flex gap-8 text-sm">
 
