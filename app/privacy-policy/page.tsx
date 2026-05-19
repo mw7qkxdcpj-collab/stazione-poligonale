9432c56507ca70d4ba9062729a4dce6d3f460904
mw7qkxdcpj-collab <maurizio1999@hotmail.it>
2026-05-19 22:44:25 +0200

    modifica privacy

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -4,29 +4,31 @@ export default function PrivacyPolicyPage() {
       className="min-h-screen text-white bg-cover bg-center"
       style={{ backgroundImage: "url('/sfondo.jpg')" }}
     >
-    <main className="min-h-screen bg-black text-white px-6 py-20">
-      <div className="max-w-3xl mx-auto">
+      <div className="min-h-screen bg-black/70 px-6 py-20">
 
-        <h1 className="text-4xl font-bold mb-8">
-          Privacy e Cookie Policy
-        </h1>
+        <div className="max-w-3xl mx-auto">
 
-        <p className="text-zinc-300 leading-8 mb-6">
-          Questo sito può raccogliere dati anonimi di navigazione
-          tramite cookie tecnici e strumenti statistici.
-        </p>
+          <h1 className="text-4xl font-bold mb-8">
+            Privacy e Cookie Policy
+          </h1>
 
-        <p className="text-zinc-300 leading-8 mb-6">
-          Nessun dato personale viene ceduto a terze parti.
-        </p>
+          <p className="text-zinc-300 leading-8 mb-6">
+            Questo sito può raccogliere dati anonimi di navigazione
+            tramite cookie tecnici e strumenti statistici.
+          </p>
 
-        <p className="text-zinc-300 leading-8">
-          Per richieste o informazioni:
-          tuaemail@example.com
-        </p>
+          <p className="text-zinc-300 leading-8 mb-6">
+            Nessun dato personale viene ceduto a terze parti.
+          </p>
+
+          <p className="text-zinc-300 leading-8">
+            Per richieste o informazioni:
+            tuaemail@example.com
+          </p>
+
+        </div>
 
       </div>
-      </main>
     </main>
   );
 }
